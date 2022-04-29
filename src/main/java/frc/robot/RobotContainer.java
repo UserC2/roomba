3b5487cf215ff48d80573c380c2d7fe519db80df
Aydan Boynton <aj01cars@outlook.com>
2022-04-29 17:46:47 -0400

    Cleanup RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,46 +4,28 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.Drive;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.HolonomicDrive;
+import frc.robot.subsystems.Drive;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
+// Structure of the robot (subsystems, commands, and button) goes in here
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final XboxController m_stick = new XboxController(0);
-
-  private final Drive drivesubsystem = new Drive(m_stick);
+  public static final XboxController m_stick = new XboxController(Constants.Xbox.DRIVER_PORT);
+  public static final Drive m_driveSubsystem = new Drive(m_stick);
+  
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
-    
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
+  // Configure button bindings here
   private void configureButtonBindings() {}
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  // public Command getAutonomousCommand() {
-  //   // An ExampleCommand will run in autonomous
-  //   return null;
-  // }
+  // Use this for auto chooser
+  public Command getAutonomousCommand() {
+    return null;
+  }
 }
