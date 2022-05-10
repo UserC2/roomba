c504560d15faa90baca4207ee591feac5fec7b9c
WhyNot180 <bence.szucs200625@gmail.com>
2022-05-10 15:26:13 -0400

    fixes spark channels

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,23 +5,27 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
 
     // TEST ALL OF THESE AGAIN
     public final class PWM {
-        public static final int LEFT_SPARK_CHANNEL = 2;
-        public static final int RIGHT_SPARK_CHANNEL = 5;
-        public static final int FRONT_LEFT_SPARK_CHANNEL = 3;
-        public static final int FRONT_RIGHT_SPARK_CHANNEL = 0;
-        public static final int REAR_LEFT_SPARK_CHANNEL = 1;
-        public static final int REAR_RIGHT_SPARK_CHANNEL = 4;
+        public static final int LEFT_SPARK_CHANNEL = 1;
+        public static final int RIGHT_SPARK_CHANNEL = 4;
+        public static final int FRONT_LEFT_SPARK_CHANNEL = 0;
+        public static final int FRONT_RIGHT_SPARK_CHANNEL = 3;
+        public static final int REAR_LEFT_SPARK_CHANNEL = 2;
+        public static final int REAR_RIGHT_SPARK_CHANNEL = 5;
     }
 
     public final class Xbox {
